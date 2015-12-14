814e748ec352a5551eb5323851c2feaa02987a02
ozgesonmez557 <ozge_linkes_94@windowslive.com>
2015-12-15 00:20:16 +0200

    frek.m dosyası eklendi

---
+++ frek.m
@@ -0,0 +1,18 @@
+function ff=frek (nota,oktav)
+notalar=['A' 'a' 'B' 'b' 'C' 'c' 'D' 'd' 'E' 'e' 'F' 'f']; %notalarý notalar a atadým
+referans=16.35; % iþlem yaparken referansýmý olarak belirledim
+p=length(notalar); %notalarýn uzunluðu kadar olan p yi oluþturdum
+
+
+for okt=0:8 %okt deðeri 0 dan 8 e kadar 1 er arttýrarak 0 dan 8 e kadar oktavlarý oluþturdum
+    if okt==oktav %oktav deðerini okt deðerine atadým
+        
+        for j=1:p%j yi 1 den baþlatýp p yani notalarýn uzunluðu kadar devam ettir
+            if nota==char (notalar(j))%notalarýn karakter deðerini nota ya atadým
+                
+                ff=2^okt*(2^((j-1)/12)*referans);%referans ý esas alarak frekans deðerlerini hesaplattým
+            end
+        end
+    end
+end
+end
