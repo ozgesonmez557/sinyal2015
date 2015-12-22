e3373bf53da3a9ba99b8d92c05d4e2aed688742a
ozgesonmez557 <ozge_linkes_94@windowslive.com>
2015-12-22 08:34:10 +0200

    Update frek.m

--- frek.m
+++ frek.m
@@ -1,19 +1,28 @@
-function ff=frek (nota,oktav)
-notalar=['C','c','D','E','e','F','f','G','g','A','b','B']; %notaları notalar a atadım
-% C=C(Do) c=C# D=D(Re) E=Eb e=E(Mi) F=F(Fa) f=F# G=G(Sol) g=G# A=A(La) b=Bb B=B(Si) şeklinde isimlendirdim.
+function ff=frek (nota,oktav) %nota ve oktav değerlerini alacak
+notalar={'Do','Dod','Re','Mib','Mi','Fa','Fad','Sol','Sold','La','Sib','Si','sus'}; %notaları notalar a atadım
+
 referans=16.35; % işlem yaparken referansımı olarak belirledim
-p=length(notalar); %notaların uzunluğu kadar olan p yi oluşturdum
+k=length(notalar); %notaların uzunluğu kadar olan k yı oluşturdum
+susf=0;
+if nargin<2
+     oktav=4;
+end
 
-
-for okt=0:8 %okt değeri 0 dan 8 e kadar 1 er arttırarak 0 dan 8 e kadar oktavları oluşturdum
-    if okt==oktav %oktav değerini okt değerine atadım
+for i=0:8 %okt değeri 0 dan 8 e kadar 1 er arttırarak 0 dan 8 e kadar oktavları oluşturdum
+    if i==oktav %i nin değeri oktavın değerine eşitse if'in içine gir
         
-        for j=1:p%j yi 1 den başlatıp p yani notaların uzunluğu kadar devam ettir
-            if nota==char (notalar(j))%notaların karakter değerini nota ya atadım
-                
-                ff=2^okt*(2^((j-1)/12)*referans);%referans ı esas alarak frekans değerlerini hesaplattım
+        for j=1:k%j yi 1 den başlatıp k yani notaların uzunluğu kadar devam ettir
+            if size(nota)==size(notalar{j})%notaların karakter değerini nota ya atadım
+                if nota==notalar{j}%giriş yaptığım nota,notalar dizisindeki j. elemana eşitse if'in içine gir.
+                ff=2^i*(2^((j-1)/12)*referans);%referans ı esas alarak frekans değerlerini hesaplattım
+                if size(nota)==size(notalar{13})
+                if nota==notalar{13}
+                    ff=0;
+                end
             end
         end
     end
+end
+end
 end
 end
