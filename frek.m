d12d16f1771efb6c06fd8ed343e579f551b649de
ozgesonmez557 <ozge_linkes_94@windowslive.com>
2016-01-05 14:23:25 +0200

    Update frek.m

--- frek.m
+++ frek.m
@@ -1,13 +1,13 @@
 function ff=frek(nota,oktav) %girilen nota ve oktav degerine gore frekansi hesaplayan fonksiyon tanimi
-notalar={'Do','Dod','Re','Red','Mi','Fa','Fad','Sol','Sold','La','Lad','Si','sus'};%Lad=La#,Sib;Dod=Do#,Reb;Red=Re#,Mib;Fad=Fa#,Solb;Sold=Sol#,Lab
+notalar={'do','dod','re','red','mi','fa','fad','sol','sold','la','lad','si','sus'};%Lad=La#,Sib;Dod=Do#,Reb;Red=Re#,Mib;Fad=Fa#,Solb;Sold=Sol#,Lab
 referans=16.35;    %formul icin ilk deger atamasi
 p=length(notalar); %notalar dizisinin eleman sayisi 
-if nargin<2        %girilen arguman sayısının 2'den az olma durumu
+if nargin<2        %girilen arguman sayÄ±sÄ±nÄ±n 2'den az olma durumu
     oktav=4;
 end
 for okt=0:8        %okt(oktav) degerini 0'dan 8(dahil)'e kadar artirma islemi
     if okt==oktav  %girilen oktav degerinin okt degeriyle esitlenmesi halinde
-        for j=1:p  %++dizi eleman indisinin artırımı
+        for j=1:p  %++dizi eleman indisinin artÄ±rÄ±mÄ±
             if size(nota)==size(notalar{j}) 
                 if nota==notalar{j}          
                     ff=2^okt*(2^((j-1)/12)*referans); 
@@ -23,4 +23,3 @@ for okt=0:8        %okt(oktav) degerini 0'dan 8(dahil)'e kadar artirma islemi
     end
 end
 end
-
